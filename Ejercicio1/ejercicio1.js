35152862272c46a66933cda21ecd496c907673d1
mariagis <m.cortegoso.garcia@gmail.com>
2021-01-30 13:37:05 +0100

    Ejercicio1

---
+++ Ejercicio1/ejercicio1.js
@@ -0,0 +1,15 @@
+const volumen = 16*23*13
+console.log('volumen', volumen + " cm3")
+
+const areaLateral = 23*13
+const areaTotal = areaLateral + 2*16*23
+console.log('areaTotal', areaTotal + "cm2")
+
+const base = 23
+const altura = 13
+const ancho = 16
+const volumen1= base*altura*ancho
+console.log('volumen1', volumen1)
+
+const areaTotal1= base*altura + 2*base*ancho
+console.log('areaTotal1', areaTotal1)
