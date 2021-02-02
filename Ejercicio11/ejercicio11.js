e40c6e645204e0cbc03a565f6ac93eb3f54962b9
mariagis <m.cortegoso.garcia@gmail.com>
2021-02-02 13:46:57 +0100

    Ejercicio11

---
+++ Ejercicio11/ejercicio11.js
@@ -0,0 +1,6 @@
+const times = [60, 75, 79, 80, 55, 59]
+const suma = times.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+
+const media = suma/times.length
+
+console.log ("media", media)
