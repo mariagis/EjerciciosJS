b3b841ee59a5bf7e4edc561b205424cc7b1126ed
mariagis <m.cortegoso.garcia@gmail.com>
2021-02-02 12:00:55 +0100

    Ejercicio2

---
+++ Ejercicio2/ejercicio2.js
@@ -0,0 +1,8 @@
+const cumple = {
+    dia: 7,
+    mes:'diciembre'
+}
+console.log('Mi cumpleaños es el ' + cumple.dia + ' de ' + cumple.mes)
+
+const meses= ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+console.log(meses[0])
