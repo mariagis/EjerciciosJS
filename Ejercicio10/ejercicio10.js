6f80a7950eded61b8141c2cfbbd6f2db2f2b2e0e
mariagis <m.cortegoso.garcia@gmail.com>
2021-02-02 14:18:29 +0100

    Ejercicio10

--- Ejercicio10/ejercicio10.js
+++ Ejercicio10/ejercicio10.js
@@ -2,4 +2,5 @@ const numbers = [253, 8575, 1, 20, 562, 1233, 25, 27, 258, 254, 7485, 2683]
 const pares = numbers.filter(number => number%2 == 0)
 const impares = numbers.filter (number => number%2 > 0)
 console.log("Pares", pares)
+
 console.log ("Impares", impares)
