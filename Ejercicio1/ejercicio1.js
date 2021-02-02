8dbd97bede4b71d592462acb9253688ccf0a78ac
mariagis <m.cortegoso.garcia@gmail.com>
2021-02-02 12:00:46 +0100

    Ejercicio1

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -3,7 +3,7 @@ console.log('volumen', volumen + " cm3")
 
 const areaLateral = 23*13
 const areaTotal = areaLateral + 2*16*23
-console.log('areaTotal', areaTotal + "cm2")
+console.log('areaTotal', areaTotal + " cm2")
 
 const base = 23
 const altura = 13
