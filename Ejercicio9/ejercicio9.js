b5d5d4885e3588dac05c50c68ed2a57b922e4a66
mariagis <m.cortegoso.garcia@gmail.com>
2021-02-02 13:28:13 +0100

    Ejercicio9

---
+++ Ejercicio9/ejercicio9.js
@@ -0,0 +1,6 @@
+const topics =["JavaScript", "Variables", "funciones", "condicionales", "bucles"]
+const upperTopics = topics.map(topic => topic.toUpperCase())
+
+upperTopics.reverse()
+
+console.log("upperTopics", upperTopics)
