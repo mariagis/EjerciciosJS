7116cbafd1585ef6f0e0583c02a05b0c90187958
mariagis <m.cortegoso.garcia@gmail.com>
2021-02-02 12:01:02 +0100

    Ejercicio3

---
+++ Ejercicio3/ejercicio3.js
@@ -0,0 +1,9 @@
+const data = ["hola", 2, 5, "adios"]
+
+console.log("5 es mayor que 2", data[2]>data[1])
+
+console.log("Suma", data[1]+data[2])
+console.log("Resta", data[1]-data[2])
+console.log("Multiplicación", data[1]*data[2])
+console.log("División", data[1]/data[2])
+console.log("Resto", data[1]%data[2])
