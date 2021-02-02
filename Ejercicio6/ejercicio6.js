bfa0d975edc9606483a0c70ce29b298ab6b8b9f8
mariagis <m.cortegoso.garcia@gmail.com>
2021-02-02 12:01:26 +0100

    Ejercicio6

---
+++ Ejercicio6/ejercicio6.js
@@ -0,0 +1,13 @@
+const notas =[6, 7, 2, 9, 3, 4, 5, 8, 2, 5, 7]
+
+let acc = 0
+for (let i = 0; i < notas.length; i++) {
+    acc += notas [i];
+}
+console.log("Con el bucle for la puntuación final es " + acc)
+
+let puntuacion = 0
+for (let nota of notas) {
+    puntuacion += nota;
+}
+console.log("Con el bucle for-of la puntuación final es " + puntuacion)
